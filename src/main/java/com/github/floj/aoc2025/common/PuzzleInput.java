04a35b4968aa50b7cccb835991d1bf910e28c958
floj <floj@users.noreply.github.com>
2025-12-06 12:35:16 +0100

    formating

--- src/main/java/com/github/floj/aoc2025/common/PuzzleInput.java
+++ src/main/java/com/github/floj/aoc2025/common/PuzzleInput.java
@@ -4,23 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.LinkedList;
 
 public class PuzzleInput {
 
-	public static List<String> readAllLines(String resource) throws IOException {
-		try (var rin = PuzzleInput.class.getResourceAsStream(resource);
-				var isr = new InputStreamReader(rin, StandardCharsets.UTF_8);
-				var br = new BufferedReader(isr)) {
-			return br.readAllLines();
-		}
-	}
+  public static LinkedList<String> readAllLines(String resource) throws IOException {
+    try (var rin = PuzzleInput.class.getResourceAsStream(resource);
+        var isr = new InputStreamReader(rin, StandardCharsets.UTF_8);
+        var br = new BufferedReader(isr)) {
+      var lines = br.readAllLines();
+      return new LinkedList<>(lines);
+    }
+  }
 
-	public static String readString(String resource) throws IOException {
-		try (var rin = PuzzleInput.class.getResourceAsStream(resource);
-				var isr = new InputStreamReader(rin, StandardCharsets.UTF_8);
-				var br = new BufferedReader(isr)) {
-			return br.readAllAsString();
-		}
-	}
+  public static String readString(String resource) throws IOException {
+    try (var rin = PuzzleInput.class.getResourceAsStream(resource);
+        var isr = new InputStreamReader(rin, StandardCharsets.UTF_8);
+        var br = new BufferedReader(isr)) {
+      return br.readAllAsString();
+    }
+  }
 }
